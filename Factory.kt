a7ffba27d2a8b4ec121f322e4f0d975ec606b4ff
Raheem <67774375+raheemadamboev@users.noreply.github.com>
2021-08-02 16:29:06 +0500

    Factory pattern

---
+++ Factory.kt
@@ -0,0 +1,25 @@
+/**
+ * Factory pattern -> Getting one from multiple options
+ */
+enum class DialogType {
+    DIALOG_CREATE_CHAT,
+    DIALOG_DELETE_MESSAGE,
+    DIALOG_EDIT_MESSAGE
+}
+
+sealed class Dialog {
+    object CreateChatDialog : Dialog()
+    object DeleteMessageDialog: Dialog()
+    object EditMessageDialog: Dialog()
+}
+
+object DialogFactory {
+
+    fun createDialog(dialogType: DialogType): Dialog {
+        return when(dialogType) {
+            DialogType.DIALOG_CREATE_CHAT -> Dialog.CreateChatDialog
+            DialogType.DIALOG_DELETE_MESSAGE -> Dialog.DeleteMessageDialog
+            DialogType.DIALOG_EDIT_MESSAGE -> Dialog.EditMessageDialog
+        }
+    }
+}
